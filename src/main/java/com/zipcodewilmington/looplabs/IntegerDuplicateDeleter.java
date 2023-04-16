e9788f657f91d12cd5a7eeddf2d6025fb9921a58
Nina <nina.emiko.mcnair@gmail.com>
2023-04-16 19:45:25 -0400

    TGFStreams babyy

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,12 +15,26 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
     }
     @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications){
-
-        return new Integer[0];
+        Integer[] intArray = Arrays.stream(this.array)
+                .filter(num -> Collections.frequency(Arrays.asList(this.array), num) != exactNumberOfDuplications)
+                .toArray(Integer[]::new);
+        return intArray;
     }
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] intArray = Arrays.stream(this.array)
+                .filter(num -> Collections.frequency(Arrays.asList(this.array), num) < maxNumberOfDuplications)
+                .toArray(Integer[]::new);
+        return intArray;
+    }
 
-        return new Integer[0];
+    public static int getOccurances(Integer[] arr, int num) {
+        int count = 0;
+        for (int i : arr) {
+            if (num == i) {
+                count++;
+            }
+        }
+        return count;
     }
 }
